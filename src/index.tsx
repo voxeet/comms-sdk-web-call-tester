35037873ca2ae62146789846161e9077b5abb402
Fabien Lavocat <fabien.lavocat@dolby.com>
2023-02-14 17:57:24 -0800

    Update index.tsx
    Signed-off-by: Fabien Lavocat <fabien.lavocat@dolby.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import { createRoot } from 'react-dom/client';
 import Home from './components/home';
 
 const container = document.getElementById('root');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!);
 root.render(<Home />);
